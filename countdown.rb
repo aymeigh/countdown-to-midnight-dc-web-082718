f180f32056a2795d3d49f781cd1ec70b0d466107
Amy Zhou <aymeighz@gmail.com>
2018-08-13 17:30:31 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,6 +5,15 @@ def countdown(number)
 while x < 10
   puts "#{number} SECOND(S)!"
   x -= 1
+  puts "HAPPY NEW YEAR!"
+end
+end
+
+def countdown_with_sleep(number)
+  x = 10
+while x < 10
+  puts "#{number} SECOND(S)!"
+  x -= 1
 end
 puts "HAPPY NEW YEAR!"
 end
